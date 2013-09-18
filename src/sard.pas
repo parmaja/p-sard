050f543aac56a888aa94a7b23df31fe756d17130
Zaher Dirkey <zaherdirkey@yahoo.com>
2013-09-18 11:59:11 +0300

    Use sard.pas as main executer utils

--- src/sard.pas
+++ src/sard.pas
@@ -15,12 +15,63 @@ unit sard;
 interface
 
 uses
-  Classes, SysUtils, Contnrs;
+  Classes, SysUtils, Contnrs,
+  sardClasses, sardScripts;
+
+type
+
+  { TmyScanner }
+
+  TmyParser = class(TsardScriptParser)
+  protected
+    procedure Push(Token: String; TokenID: Integer); override;
+  end;
+
+  { TmyScript }
+
+  TmyScript = class(TsardScript)
+  protected
+    function CreateParser: TsardParser; override;
+  end;
+
+function Execute(Lines: TStrings): Boolean;
 
 implementation
 
 uses
   StrUtils;
+
+{ TmyScript }
+
+function TmyScript.CreateParser: TsardParser;
+begin
+  Result := TmyParser.Create;
+end;
+
+{ TmyScanner }
+
+procedure TmyParser.Push(Token: String; TokenID: Integer);
+begin
+  inherited;
+  WriteLn(Token);
+end;
+
+function Execute(Lines: TStrings): Boolean;
+var
+  Scanner: TmyScript;
+  Element: TsardElement;
+begin
+  Scanner := TmyScript.Create;
+  Element:= TsardElement.Create(nil);
+  try
+    Scanner.IntoElement := Element;
+    Scanner.Scan(Lines);
+  finally
+    Element.Free;
+    Scanner.Free;
+  end;
+  Result := True;
+end;
 
 end.
 
